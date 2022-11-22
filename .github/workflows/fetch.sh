63705b7fb3efa5f15816e72c0b66d7d3701667e5
vpereira01 <42812459+vpereira01@users.noreply.github.com>
2022-11-22 22:31:48 +0000

    Adds fetch script

---
+++ .github/workflows/fetch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+baseDestDir=./data/raw
+destDir=$baseDestDir/$(date +%Y%m%d_%H%M%S)
+mkdir $destDir
+startParameter=0
+
+reqNum=1
+while true
+do
+    fetchURL="https://e-redes.opendatasoft.com/api/records/1.0/search/?dataset=outages-per-geography&q=&facet=zipcode&facet=municipality&start=$startParameter"
+    oFileName=$destDir/$( printf '%09d' $reqNum ).json
+
+    curl "$fetchURL" -o $oFileName
+    curNHits=$(jq '.nhits' $oFileName)
+    curRows=$(jq '.parameters.rows' $oFileName)
+    curStart=$(jq '.parameters.start' $oFileName)
+
+    startParameter=$((curStart+curRows))
+    if (( $startParameter > $curNHits )); then
+        break;
+    fi
+
+    sleep 0.2
+    ((reqNum=reqNum+1))
+done
---
+++ data/raw/README.md
@@ -0,0 +1,3 @@
+# Read me!
+
+This directory is meant to be automatically updated.
