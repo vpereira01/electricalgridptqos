62032922a703627f235d993da874984fcf8baf46
vpereira01 <42812459+vpereira01@users.noreply.github.com>
2023-08-22 22:10:34 +0100

    fix raw2records failing on empty files

--- .github/workflows/raw2records.sh
+++ .github/workflows/raw2records.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
+if ! compgen -G "data/raw/*/*.json" > /dev/null; then
+    echo "no files to process, exiting"
+    exit 1
+fi
+
 jq -r '.records[] | [.recordid, .record_timestamp, .fields.extractiondatetime, .fields.zipcode, .fields.municipality] | @csv ' data/raw/*/*.json >> data/records/all.csv
 rm -r data/raw/*/*.json
