e45e5600a9830930ec04ff153255a80eb4bd1231
vpereira01 <42812459+vpereira01@users.noreply.github.com>
2023-08-23 21:18:08 +0100

    fix raw2records exit code

--- .github/workflows/raw2records.sh
+++ .github/workflows/raw2records.sh
@@ -2,7 +2,7 @@
 
 if ! compgen -G "data/raw/*/*.json" > /dev/null; then
     echo "no files to process, exiting"
-    exit 1
+    exit 0
 fi
 
 jq -r '.records[] | [.recordid, .record_timestamp, .fields.extractiondatetime, .fields.zipcode, .fields.municipality] | @csv ' data/raw/*/*.json >> data/records/all.csv
