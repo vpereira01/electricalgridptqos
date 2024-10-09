f03973296dc7eec093d6d88b592a664b6d72bc33
vpereira01 <42812459+vpereira01@users.noreply.github.com>
2024-10-09 15:44:23 +0100

    improve fetch output

--- .github/workflows/fetch.sh
+++ .github/workflows/fetch.sh
@@ -6,25 +6,36 @@ mkdir $destDir
 startParameter=0
 
 reqNum=1
-while true
-do
-    fetchURL="https://e-redes.opendatasoft.com/api/records/1.0/search/?dataset=outages-per-geography&q=&facet=zipcode&facet=municipality&start=$startParameter"
-    oFileName=$destDir/$( printf '%09d' $reqNum ).json
+echo "Starting data fetch process..."
 
-    curl "$fetchURL" -o $oFileName
+while true; do
+    fetchURL="https://e-redes.opendatasoft.com/api/records/1.0/search/?dataset=outages-per-geography&q=&facet=zipcode&facet=municipality&start=$startParameter"
+    oFileName=$destDir/$(printf '%09d' $reqNum).json
+
+    echo "Fetching data (request #$reqNum)"
+    curl -s "$fetchURL" -o $oFileName
+    echo "Data saved to $oFileName"
+
     curNHits=$(jq '.nhits' $oFileName)
     curRows=$(jq '.parameters.rows' $oFileName)
     curStart=$(jq '.parameters.start' $oFileName)
 
-    startParameter=$((curStart+curRows))
-    if (( $startParameter > $curNHits )); then
-        break;
+    startParameter=$((curStart + curRows))
+
+    if (($startParameter > $curNHits)); then
+        echo "All records fetched successfully."
+        break
     fi
 
     sleep 0.2
-    ((reqNum=reqNum+1))
+    ((reqNum = reqNum + 1))
 done
 
 # update latest records csv
-echo "\".recordid\",\".record_timestamp\",\".fields.extractiondatetime\",\".fields.zipcode\",\".fields.municipality\"" > data/records/latest.csv
-jq -r '.records[] | [.recordid, .record_timestamp, .fields.extractiondatetime, .fields.zipcode, .fields.municipality] | @csv ' $destDir/*.json >> data/records/latest.csv
+latestRecordsCSV=./data/records/latest.csv
+
+echo "Re-generating $latestRecordsCSV..."
+echo "\".recordid\",\".record_timestamp\",\".fields.extractiondatetime\",\".fields.zipcode\",\".fields.municipality\"" > $latestRecordsCSV
+jq -r '.records[] | [.recordid, .record_timestamp, .fields.extractiondatetime, .fields.zipcode, .fields.municipality] | @csv ' $destDir/*.json >> $latestRecordsCSV
+
+echo "Successfully re-generated $latestRecordsCSV"
